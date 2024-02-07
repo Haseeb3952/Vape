830f036d256e8930e4f200b551d7903e79fce4d9
Abdul Haseeb <haseeb7063952@gmail.com>
2024-02-07 11:38:45 +0500

    update

--- assets/script/specification.js
+++ assets/script/specification.js
@@ -1,32 +1,3 @@
-function verifyAge() {
-  // Check if the user is 18 or older (you can implement your age verification logic here)
-  var isOver18 = true; // Replace this with your actual age verification logic
-
-  if (isOver18) {
-    // If the user is 18 or older, hide the age verification pop-up
-    document.querySelector('.age-verification').style.display = 'none';
-  } else {
-    // If the user is under 18, close the website
-    closeWebsite();
-  }
-}
-
-function closeWebsite() {
-  // Close the website
-  window.close();
-}
-
-
-// Counter Button 
-
-let currentValue = 0;
-
-function updateValue(amount) {
-  currentValue += amount;
-  document.getElementById('display').innerText = currentValue;
-}
-
-
 // Product Images Gallery 
 
 let product_img = document.querySelector('.product-img img');
